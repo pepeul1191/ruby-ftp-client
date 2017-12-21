379d8d93b5fc46ade8f0c671ef040d019edcf9db
Pepe Valdivia <jvaldivia@softweb.pe>
2017-12-20 23:29:09 -0500

    Carga por FTP ok

--- app.rb
+++ app.rb
@@ -4,11 +4,21 @@ server = "192.168.1.51"
 user = "pepe"
 password = "kiki123"
 
-ftp = Net::FTP.new(server)
-ftp.passive = true
-ftp.login user, password
-files = ftp.chdir('Documentos/py2/archivos')
-files = ftp.list
-puts "list out of directory:"
-puts files
-ftp.close
+Net::FTP.open(server) do |ftp|
+  begin
+    ftp.login('ftp-user', 'ftp-pass')
+    """
+    files = ftp.chdir('Documentos/py2/archivos')
+    files = ftp.list
+    puts 'list out of directory:'
+    puts files
+    """
+    origin_file = File.new('/home/pepe/Descargas/JSF 2.0 Cookbook.pdf')
+    destination_file = 'Descargas/JSF 2.0 Cookbook.pdf'
+    ftp.put(origin_file, destination_file)
+  rescue => e
+    puts e
+  ensure
+    ftp.close
+  end
+end
